ab584c32bddd6708835e3b5b1bd5ce18e7bdb281
kendras05 <kendrapsy@gmail.com>
2020-01-18 13:19:37 -0800

    added routes and content for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+  helper_method :current_user
+
 end
+
